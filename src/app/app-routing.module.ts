9858ac5180722064f81e8d07bb82fd4d3e1a00f0
Cassie Cecott <casscecott@gmail.com>
2020-06-24 08:37:21 -0400

    first commit, maybe do that more

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "main-page", component: MainPageComponent },
+  { path: "contact-me", component: ContactMeComponent },
+  { path: "projects", component: ProjectsComponent },
+  { path: "about-me", component: AboutMeComponent },
+  { path: "", redirectTo: "/main-page", pathMatch: "full" },
+  { path: "**", redirectTo: "/main-page", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+// import { PdfViewerModule } from 'ng2-pdf-viewer';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ContactMeComponent,
+    ProjectsComponent,
+    AboutMeComponent
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
